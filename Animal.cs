03211a0235d17e584b0478ebebf15842050603cb
MMGhaempanah2005 <mohammadmahdighaempanah209@gmail.com>
2025-01-25 21:22:51 +0330

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZOO
+{
+     class Animal
+    {
+        public string name;
+        public string species;
+        public int age;
+        public string specialfeatures;
+        public int numbers;
+
+
+        public Animal(string name, string species, int age, string specialfeatures, int numbers)
+        {
+            this.name = name;
+            this.species = species;
+            this.age = age;
+            this.specialfeatures = specialfeatures;
+            this.numbers = numbers;
+        }
+    }
+}
---
+++ ZOO.cs
@@ -0,0 +1,36 @@
+﻿
+using ZOO;
+
+Console.WriteLine("How many types of Animals do you have?");
+int number  = Convert.ToInt32(Console.ReadLine());
+Animal[] animals = new Animal[number];
+
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine($"please enter Animal[{(i+1)}] name :");
+    string name = Console.ReadLine();
+
+    Console.WriteLine($"please enter Animal[{(i + 1)}] species :");
+    string species = Console.ReadLine();
+
+    Console.WriteLine($"please enter Animal[{(i + 1)}] age :");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"please enter Animal[{(i + 1)}] specialfeatures :");
+    string specialfeatures = Console.ReadLine();
+
+    Console.WriteLine($"please enter Animal[{(i + 1)}] numbers :");
+    int Numbers = Convert.ToInt32(Console.ReadLine()); 
+
+
+    Animal a = new Animal(name, species, age, specialfeatures, number);
+    animals[i]= a;
+
+}
+Console.ForegroundColor = ConsoleColor.Green;
+
+foreach (Animal animal in animals)
+{
+    Console.WriteLine($"name:{animal.name} species:{animal.species} age:{animal.age} specialfeatures:{animal.specialfeatures} numbers:{animal.numbers}");
+}
+Console.ResetColor();
